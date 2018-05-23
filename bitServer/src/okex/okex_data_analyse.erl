d4809998ff1126787a24ce67dd8d6415767bc2cd
xiaohui <xiaohui@163.com>
2018-05-23 01:26:02 -0400

    test

--- bitServer/src/okex/okex_data_analyse.erl
+++ bitServer/src/okex/okex_data_analyse.erl
@@ -6,11 +6,12 @@
 
 %% 回包数据分析
 handle_data(RetJsonBin) ->
-	{array, JsonList} = mochijson:decode(RetJsonBin),
-	io:format("JsonList ----- ~p", [JsonList]),
-	Fun = fun(JsonBin) ->
-		{struct, JsonL} = JsonBin,
-		io:format("JsonL--->~p", [JsonL])
-	end,
-	lists:foreach(Fun, JsonList).
+	io:format("RetJsonBin---------recv:~p", [RetJsonBin]).
+	% {array, JsonList} = mochijson:decode(RetJsonBin),
+	% io:format("JsonList ----- ~p", [JsonList]),
+	% Fun = fun(JsonBin) ->
+	% 	{struct, JsonL} = JsonBin,
+	% 	io:format("JsonL--->~p", [JsonL])
+	% end,
+	% lists:foreach(Fun, JsonList).
 	%case proplists:get_value("ResultCode", JsonL) of
