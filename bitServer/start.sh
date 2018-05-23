4765bb152e7fa2641f4c904549b01f668414d04c
xiaohui <xiaohui@163.com>
2018-05-22 21:58:21 -0400

    all

--- bitServer/start.sh
+++ bitServer/start.sh
@@ -2,6 +2,7 @@
 source ../chaos.env
 
 CHAOS_SVR_NAME=`cat $CONFIG_FILE | grep "{node" | awk -F"'" '{print $2}'`
+echo $CHAOS_SVR_NAME
 DUMP_NAME="${CHAOS_SVR_NAME}_"$DUMP_NAME
 
 ERL_PARAM=" +P 300000"
@@ -14,7 +15,7 @@ ERL_PARAM+=" -setcookie ${COOKIE}"
 ERL_PARAM+=" -smp enable"
 ERL_PARAM+=" +K true +S 4 +c"
 # ERL_PARAM+=" +zdbbl 2048"
-#ERL_PARAM+=" -detached"
+ERL_PARAM+=" -detached"
 
 echo "start okex_game_svr ..."
 ERL_CRASH_DUMP=${DUMP_DIR} INSTANCE_NAME=${DUMP_NAME} erl -boot start_sasl -config ${LOG_HOME}/log ${ERL_PARAM} -s bitServer_app
